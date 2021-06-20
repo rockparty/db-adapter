72d061a12966ceb25618bb6a48ee1ec25ee5a061
William Fank Thomé <willegp88@gmail.com>
2021-06-20 12:33:00 -0300

    fix(mongodb): payload must be cloned to prevent changes

--- src/adapters/mongodb/functions/insert-one-in-mongodb.ts
+++ src/adapters/mongodb/functions/insert-one-in-mongodb.ts
@@ -2,6 +2,7 @@ import type {
   InsertOneFn,
   InsertOneFnArgs,
 } from '@/protocols/factories/functions/insert-one-factory.protocol'
+import { clone } from '@/utils'
 import type { CollectionInsertOneOptions, MongoClient } from 'mongodb'
 
 export function insertOneInMongodb(
@@ -11,7 +12,7 @@ export function insertOneInMongodb(
   return async function <T>(args: InsertOneFnArgs<T>): Promise<T> {
     const { in: collectionName, as: payload } = args
 
-    await client.db().collection(collectionName).insertOne(payload, opts)
+    await client.db().collection(collectionName).insertOne(clone(payload), opts)
 
     return payload
   }
--- src/factories/functions/insert-one.factory.ts
+++ src/factories/functions/insert-one.factory.ts
@@ -3,16 +3,11 @@ import type {
   InsertOneFactory,
   InsertOneFactoryArgs,
 } from '@/protocols/factories/functions/insert-one-factory.protocol'
-import { clone } from '@/utils'
 
 export function insertOneFactory(opts: InsertOneFactoryArgs): InsertOneFactory {
   const { db } = opts
   return async function <T>(args: InsertOneFnArgs<T>): Promise<T> {
-    const payload = clone(args.as)
-    const data = await db.insertOne({
-      ...args,
-      as: payload,
-    })
-    return data
+    const insertedData = await db.insertOne(args)
+    return insertedData
   }
 }
