3e36e96ce6da88a2678abc95f5b1b635038db3ae
William Fank Thomé <willegp88@gmail.com>
2021-06-20 14:34:03 -0300

    feat(firestore): get one

---
+++ src/adapters/firestore/functions/get-one-from-firestore.ts
@@ -0,0 +1,20 @@
+import type {
+  GetOneFn,
+  GetOneFnArgs,
+} from '@/protocols/adapters/functions/get-one-function.protocol'
+import type { firestore } from 'firebase-admin/lib/firestore'
+
+export function getOneFromFirestore(db: firestore.Firestore): GetOneFn {
+  return async function <T>(args: GetOneFnArgs<T>): Promise<T | null> {
+    const { from: collectionName, by: key, matching: value } = args
+
+    const result = await db
+      .collection(collectionName)
+      .where(key, '==', value)
+      .get()
+
+    const founded = result.docs[0]?.exists ? (result.docs[0].data() as T) : null
+
+    return founded
+  }
+}
