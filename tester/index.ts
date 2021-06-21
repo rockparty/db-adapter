1ab52e6a85e137730134526cd9b37a36e48e3ad7
William Fank Thomé <willegp88@gmail.com>
2021-06-21 13:19:24 -0300

    feat: test lib

--- tester/index.ts
+++ tester/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello world')
+import dbAdapter from '@rockparty/db-adapter'
+
+dbAdapter
+  .mongodbAdapter({
+    uri: 'mongodb://localhost:27001/db-adapter',
+    clientOpts: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  })
+  .then(async (db) => {
+    const inserted = await db.insertOne({
+      in: 'test',
+      as: {
+        foo: 'bar',
+      },
+    })
+
+    const all = await db.getAll({
+      from: 'test',
+    })
+
+    console.log(inserted, all)
+  })
