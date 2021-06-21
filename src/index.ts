d6e36ea18057ceb0f695dc0b2a1152ea2c9390c7
William Fank Thomé <willegp88@gmail.com>
2021-06-21 12:45:37 -0300

    refactor: index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello World')
+import { mongodbDbAdapter as mongodbAdapter } from './adapters/mongodb/mongo-db.db-adapter'
+import { firestoreDbAdapter as firestoreAdapter } from './adapters/firestore/firestore.db-adapter'
+import * as protocols from './protocols'
+
+export default {
+  mongodbAdapter,
+  firestoreAdapter,
+  protocols,
+}
